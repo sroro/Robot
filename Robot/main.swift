1f504000045b4947478f588a8d6b20445e69a5f6
roro <rodolphe.schnetzer@gmail.com>
2019-09-04 17:18:21 +0200

    Initial Commit

---
+++ Robot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Robot
+//
+//  Created by Rodolphe Schnetzer on 04/09/2019.
+//  Copyright © 2019 Rodolphe Schnetzer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
